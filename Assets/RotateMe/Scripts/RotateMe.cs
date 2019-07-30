59029301a00e97afdfca3334bc7df0f4264e81ef
mob-sakai <sakai@mobcast.jp>
2019-07-30 17:58:12 +0900

    feat: hogehoge!
    BREAKING CHANGE: Debug code has been removed.

--- Assets/RotateMe/Scripts/RotateMe.cs
+++ Assets/RotateMe/Scripts/RotateMe.cs
@@ -9,7 +9,6 @@ namespace Coffee.RotateMe
 		{
 			var rot = Quaternion.AngleAxis (angularVelocity * Time.deltaTime, Vector3.up);
 			transform.localRotation = rot * transform.localRotation;
-			Debug.Log("test");
 		}
 	}
 }
